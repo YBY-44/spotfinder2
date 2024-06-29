2d17e90302af486087aada63e542a8b9328551e4
yby <yuby0404_qd@163.com>
2024-06-29 17:37:14 +1000

    second

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import { add } from '@spotfinder2/sample-lib';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!! ' + add(10, 20);
   }
 }
