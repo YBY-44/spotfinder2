d75e0076694ea3211ef6463ec138e2a435bac00a
yby <yuby0404_qd@163.com>
2024-08-11 15:25:02 +1000

    deploy

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { add } from '@spotfinder2/sample-lib';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!! ' + add(10, 20);
+    return 'Hello World!!';
   }
   postHello(body: any): { message: string } {
     console.log('Received POST request with body:', body);
